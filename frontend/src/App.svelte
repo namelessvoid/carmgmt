78f4589b760aa7bf4209428db81b1d197f046d19
namelessvoid <simon_kerler@web.de>
2019-10-07 23:17:13 +0200

    Replace react frontend by svelte app

---
+++ frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import CarList from './CarList.svelte';
+
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+<CarList />
---
+++ frontend/src/CarList.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { onMount } from 'svelte';
+
+    let cars = []
+
+    onMount(() => {
+        fetch("http://localhost:8080/cars")
+        .then(response => response.json())
+        .then(json => cars = json);
+    });
+</script>
+<h2>Cars</h2>
+{#each cars as car}
+<div>{car.ID}: {car.Name}</div>
+{/each}
