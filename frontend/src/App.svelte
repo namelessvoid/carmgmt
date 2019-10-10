241f455236d89cdcf5c916c62df72ae093cc28ad
namelessvoid <simon_kerler@web.de>
2019-10-10 22:59:52 +0200

    Add language switcher

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,6 +2,7 @@
 	import { Router, Route } from 'svelte-routing';
 	import Fleet from './fleet/Fleet.svelte';
 	import VehicleDetail from './fleet/VehicleDetail.svelte';
+	import LanguageSwitcher from './i18n/LanguageSwitcher.svelte';
 
 	let url = "";
 </script>
@@ -13,7 +14,7 @@
 </style>
 
 <Router url={url}>
-	<h1>Hello foo!</h1>
+	<h1>Hello foo!</h1><LanguageSwitcher />
 
 	<Route path="fleet" component={Fleet} />
 	<Route path="fleet/vehicle/:vehicleId" component={VehicleDetail} />
