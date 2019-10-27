55d2ae4fe906f9ab62a7b14a35f285d2f8b7a1b4
namelessvoid <simon_kerler@web.de>
2019-10-27 22:30:50 +0100

    Add validation and enhance DatePicker tests

--- frontend/src/DatePicker.svelte
+++ frontend/src/DatePicker.svelte
@@ -1,22 +1,35 @@
 <script>
     import { onMount, createEventDispatcher } from 'svelte';
 
-    export let value = new Date();
+    export let value;
+    value.setSeconds(0);
+    value.setMilliseconds(0);
+
+    let validation = {
+        date: true,
+        time: true
+    };
 
     const dispatch = createEventDispatcher();
 
     function setDate(dateString) {
         const date = new Date(dateString);
+        validation.date = date.toString() !== 'Invalid Date';
+
         value.setFullYear(date.getFullYear());
         value.setMonth(date.getMonth());
         value.setDate(date.getDate());
+
         dispatch('changed', value);
     }
 
     function setTime(timeString) {
+        validation.time = timeString !== "";
+
         const [hours, minutes] = timeString.split(':');
         value.setHours(parseInt(hours));
         value.setMinutes(parseInt(minutes));
+
         dispatch('changed', value);
     }
 
@@ -40,5 +53,5 @@ input {
 }
 </style>
 
-<input name="date" type="date" value={getDateString(value)} on:change={(e) => setDate(e.target.value)} />
-<input name="time" type="time" value={getTimeString(value)} on:change={(e) => setTime(e.target.value)} />
+<input name="date" type="date" value={getDateString(value)} on:change={(e) => setDate(e.target.value)} class:invalid={validation.date === false} />
+<input name="time" type="time" value={getTimeString(value)} on:change={(e) => setTime(e.target.value)} class:invalid={validation.time === false} />
--- frontend/src/DatePicker.test.js
+++ frontend/src/DatePicker.test.js
@@ -20,6 +20,16 @@ describe('DatePicker', () => {
         expect(timeInput().value).toBe('14:30');
     });
 
+    it('should set seconds and milliseconds to zero', () => {
+        const date = new Date('2035-05-22T14:30:59.932+02:00');
+
+        const { component } = render(DatePicker, { props: { value: date }});
+
+        const actualDate = component.$$.ctx.value;
+        expect(actualDate.getSeconds()).toBe(0);
+        expect(actualDate.getMilliseconds()).toBe(0);
+    })
+
     it('should trigger changed event when date is changed', async () => {
         // Be aware of DST
         const date = new Date('2035-05-22T14:30:00+02:00');
@@ -47,5 +57,57 @@ describe('DatePicker', () => {
 
         expect(changeCallback).toHaveBeenCalledTimes(1);
         expect(changeCallback.mock.calls[0][0].detail).toEqual(expectedDate);
+    });
+
+    it('should trigger changed event with "Invalid Date" when invalid date is entered', async () => {
+        const changeCallback = jest.fn();
+
+        const { component } = render(DatePicker, { props: { value: new Date() }});
+        component.$on('changed', changeCallback);
+
+        await fireEvent.change(dateInput(), { target: { value: 'asdfas' }});
+
+        expect(changeCallback).toHaveBeenCalledTimes(1);
+        expect(changeCallback.mock.calls[0][0].detail.toString()).toEqual('Invalid Date');
+    });
+
+    it('should trigger changed event with "Invalid Date" when invalid time is entered', async () => {
+        const changeCallback = jest.fn();
+
+        const { component } = render(DatePicker, { props: { value: new Date() }});
+        component.$on('changed', changeCallback);
+
+        await fireEvent.change(timeInput(), { target: { value: 'asdfas' }});
+
+        expect(changeCallback).toHaveBeenCalledTimes(1);
+        expect(changeCallback.mock.calls[0][0].detail.toString()).toEqual('Invalid Date');
+    });
+
+    it('should validate input', async () => {
+        render(DatePicker, { props: { value: new Date() }});
+
+        expect(dateInput().classList).not.toContain('invalid');
+        expect(timeInput().classList).not.toContain('invalid');
+
+        await fireEvent.change(dateInput(), { target: { value: 'asdf' }});
+        expect(dateInput().classList).toContain('invalid');
+        expect(timeInput().classList).not.toContain('invalid');
+
+        await fireEvent.change(dateInput(), { target: { value: '2000-10-01' }});
+        expect(dateInput().classList).not.toContain('invalid');
+        expect(timeInput().classList).not.toContain('invalid');
+
+        await fireEvent.change(timeInput(), { target: { value: 'adf' }});
+        expect(dateInput().classList).not.toContain('invalid');
+        expect(timeInput().classList).toContain('invalid');
+
+        await fireEvent.change(timeInput(), { target: { value: '14:12' }});
+        expect(dateInput().classList).not.toContain('invalid');
+        expect(timeInput().classList).not.toContain('invalid');
+
+        await fireEvent.change(dateInput(), { target: { value: 'adsf' }});
+        await fireEvent.change(timeInput(), { target: { value: 'adf' }});
+        expect(dateInput().classList).toContain('invalid');
+        expect(timeInput().classList).toContain('invalid');
     });
 });
